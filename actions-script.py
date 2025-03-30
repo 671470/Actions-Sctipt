a9e5e6ec4dbc1f84e782fe9bff923011d3cff90f
brennuvargur <111428258+671470@users.noreply.github.com>
2025-03-30 17:11:35 +0200

    Update actions-script.py

--- actions-script.py
+++ actions-script.py
@@ -1,18 +1,15 @@
 import os
 import sys
 import argparse
-from github import Github
 import subprocess
-import requests
 
-def run_actions_importer(github_org,github_repo,github_token,gitlab_token):
-     #GitHub Actions Importer
+def run_actions_importer(github_org, github_repo, github_token, gitlab_token):
+    # GitHub Actions Importer
     migrate_command = f"""
     gh actions-importer migrate gitlab --target-url https://github.com/{github_org}/{github_repo} --output-dir tmp/migrate --namespace bachelor-vizrt --project unit-testing-class-2023-java --github-access-token {github_token} --gitlab-access-token {gitlab_token}
     """
     subprocess.run(migrate_command, shell=True, check=True)
-    print("GitHub Actins Importer completed.")
-
+    print("GitHub Actions Importer completed.")
 
 def main(gitlab_repo, github_repo, gitlab_url, github_org, gitlab_token, github_token, gitlab_group_id):
     try:
@@ -20,9 +17,12 @@ def main(gitlab_repo, github_repo, gitlab_url, github_org, gitlab_token, github_
         if not github_token or not gitlab_token:
             raise ValueError("GitHub or GitLab token is missing!")
 
-    run_actions_importer(github_org,github_repo,github_token,gitlab_token)
-    printf("Actions Importer completed")
-    
+        run_actions_importer(github_org, github_repo, github_token, gitlab_token)
+
+        print("Actions Importer completed")
+
+    except Exception as e:
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Migrate GitLab repository to GitHub.")
@@ -31,6 +31,9 @@ if __name__ == "__main__":
     parser.add_argument("--gitlab-url", required=True, help="GitLab URL")
     parser.add_argument("--gitlab-token", required=True, help="GitLab token")
     parser.add_argument("--github-token", required=True, help="GitHub token")
+    parser.add_argument("--github-org", required=True, help="GitHub organization name")  # Added
+    parser.add_argument("--gitlab-group-id", required=True, help="GitLab group ID")  # Added
+
     args = parser.parse_args()
     
     main(args.gitlab_repo, args.github_repo, args.gitlab_url, args.github_org, args.gitlab_token, args.github_token, args.gitlab_group_id)
